df19125c7e012c3776f22663cf51a4b259dcde5d
Tim Malone <tdmalone@gmail.com>
2018-03-30 00:31:27 +1100

    Add basic Packer shell provisioner for default image

---
+++ aws-tm-id-au.packer.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Update stuff.
+sudo apt-get update
+sudo apt-get upgrade --assume-yes
+
+# TODO: Set up new hostname.
+
+# TODO: Change server timezone.
+
+# TODO: Add new user.
+
+# TODO: Disable password login.
+
+# TODO: Set up Papertrail monitoring.
+
+# TODO: Install the Amazon Simple Systems Manager agent.
+
+# TODO: Clone tdmalone/dotfiles.
+
+# TODO: Install Node.js & Yarn, PHP, Python & PIP, Ruby, and Go.
+
+# TODO: Install MariaDB.
+
+# TODO: Install Travis, Heroku, LastPass, Papertrail and AWS cli's, and wp-cli.
+
+# TODO: Install Ansible, Terraform and Packer.
+
+# TODO: Install nginx & set up a vhost and php-fpm.
+
+# TODO: Set up HTTPS for nginx with a LetsEncrypt certificate.
+
+# TODO: Install and set up a local WordPress site to manage tm.id.au.
