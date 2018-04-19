87ade62003a05081434804028ef98edff539e5f5
Tim Malone <tdmalone@gmail.com>
2018-04-19 22:01:04 +1000

    Minor API Gateway config

--- api/main.tf
+++ api/main.tf
@@ -1,4 +1,27 @@
-/*
-TODO: Add config for API Gateway, Cloudfront and ACM for api.tm.id.au.
-*/
+/**
+ *
+ *
+ * @see https://www.terraform.io/docs/providers/aws/r/api_gateway_rest_api.html
+ */
+resource "aws_api_gateway_rest_api" "api" {
 
+}
+
+/**
+ *
+ *
+ * @see https://www.terraform.io/docs/providers/aws/r/api_gateway_account.html
+ */
+resource "aws_api_gateway_account" "default" {
+  cloudwatch_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/apiGatewayToCloudWatchLogs"
+}
+
+/**
+ * TODO: Enable when either import is supported, or we can safely delete and recreate the domain
+ *       while still storing mapping and ACM settings.
+ *
+ * @see https://www.terraform.io/docs/providers/aws/r/api_gateway_domain_name.html
+ */
+/*resource "aws_api_gateway_domain_name" "api-tm-id-au" {
+  domain_name = "api.tm.id.au"
+}*/
