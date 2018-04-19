a31cbd0758bc849f35445e9cb62a985a800415a9
Tim Malone <tdmalone@gmail.com>
2018-04-19 22:17:23 +1000

    WIP: Converting shell provisioner over to Ansible

--- servers/aws.tm.id.au/packer.sh
+++ servers/aws.tm.id.au/packer.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+# TODO: In progress on converting this to an Ansible playbook to be a bit more declarative.
+
 # Update stuff and install helpers.
 sudo apt-get update
 sudo apt-get upgrade --assume-yes
@@ -82,11 +84,13 @@ curl --location https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5
 unzip terraform.zip
 sudo mv terraform /usr/local/bin/terraform
 
-# Install Packer
+# Install Packer.
 # TODO: Find a way to programmatically download or get the latest version number.
 curl --location https://releases.hashicorp.com/packer/1.2.2/packer_1.2.2_linux_amd64.zip > packer.zip
 unzip packer.zip
 sudo mv packer /usr/local/bin/packer
+
+# TODO: Install Vagrant.
 
 # Install gcloud CLI & kubectl.
 # @see https://cloud.google.com/sdk/docs/#deb
@@ -118,3 +122,5 @@ Bluemix_CLI/install_bluemix_cli
 # TODO: Set up HTTPS for nginx with a LetsEncrypt certificate.
 
 # TODO: Install and set up a local WordPress site to manage tm.id.au.
+
+# TODO: Install zsh and oh-my-zsh.
